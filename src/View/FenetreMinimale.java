18577ffe72d60abda8a67b6d43a6e9f71d58b220
Léo Cartel <leo76930@gmail.com>
2018-03-25 18:36:19 +0200

    Mise à jour des View

---
+++ src/View/FenetreMinimale.java
@@ -0,0 +1,10 @@
+package View;
+
+import javax.swing.*;
+
+public class FenetreMinimale {
+    FenetreMinimale( String message )
+    {
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
