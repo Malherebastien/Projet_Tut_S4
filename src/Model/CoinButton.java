f703d4b927db46fdabc0ceab2fa826bd21b4eb0b
Léo Cartel <leo76930@gmail.com>
2018-03-25 20:10:04 +0200

    Ajout de la classe coinbutton

--- src/Model/CoinButton.java
+++ src/Model/CoinButton.java
@@ -7,7 +7,9 @@ import java.io.File;
 
 public class CoinButton extends JButton {
     private Image i;
+    private String coin;
     public CoinButton( String coin ) {
+        this.coin = coin;
         //On essaye de lire l'image
         try
         {
@@ -19,5 +21,36 @@ public class CoinButton extends JButton {
         }
         this.setIcon(new ImageIcon(i));
         this.setMargin(new Insets(0, 0, 0, 0));
+    }
+
+    public void setColor( String color )
+    {
+        switch ( color )
+        {
+            case "red" :
+                try
+                {
+                    i = ImageIO.read(new File("src/Images/imgContainer"+coin+"_red.png"));
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Le lien de l'image est invalide !");
+                }
+                this.setIcon(new ImageIcon(i));
+            break;
+        }
+    }
+
+    public void resetColor()
+    {
+        try
+        {
+            i = ImageIO.read(new File("src/Images/imgContainer"+coin+"_base.png"));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Le lien de l'image est invalide !");
+        }
+        this.setIcon(new ImageIcon(i));
     }
 }
