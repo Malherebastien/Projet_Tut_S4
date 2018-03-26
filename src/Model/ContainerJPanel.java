cd97f373f8d32c19eb2ce39ed11dfd7dbdafda8f
Provencal2 <gillianmaniez@hotmail.fr>
2018-03-26 19:37:53 +0200

    Ajout du numéro des conteneurs

--- src/Model/ContainerJPanel.java
+++ src/Model/ContainerJPanel.java
@@ -12,14 +12,34 @@ public class ContainerJPanel extends JPanel implements ActionListener {
     private ArrayList <CoinButton> alCoinButton   = new ArrayList<CoinButton>();
     public ContainerJPanel()
     {
-        this.setLayout(new GridLayout(2,2));
+        this.setLayout(new GridLayout(3,2));
         alCoinButton.add(new CoinButton("BD"));
         alCoinButton.add(new CoinButton("BG"));
         alCoinButton.add(new CoinButton("HD"));
         alCoinButton.add(new CoinButton("HG"));
-        for ( CoinButton cb : alCoinButton ) {
-            this.add(cb);
-            cb.addActionListener(this);
+        for ( int i = 0; i < 2; i++ ) {
+            this.add(alCoinButton.get(i));
+            alCoinButton.get(i).addActionListener(this);
+        }
+
+        float[] hsb = new float[3];
+        Color.RGBtoHSB(211,211,211,hsb);
+
+        JLabel vide = new JLabel("");
+        vide.setBackground(Color.getHSBColor(hsb[0],hsb[1],hsb[2]));
+        vide.setOpaque(true);
+
+        this.add(vide);
+
+        JLabel nb = new JLabel("51");
+        nb.setBackground(Color.getHSBColor(hsb[0],hsb[1],hsb[2]));
+        nb.setOpaque(true);
+
+        this.add(nb);
+
+        for ( int i = 2; i < 4; i++ ) {
+            this.add(alCoinButton.get(i));
+            alCoinButton.get(i).addActionListener(this);
         }
         this.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
         this.setVisible(true);
