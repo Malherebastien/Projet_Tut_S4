806cdb7936b1dc7828fb38ed454c885fe23c4484
La Brozone <labrozone@gmail.com>
2018-03-28 11:39:18 +0200

    Ajout des différentes images

--- src/Model/Coin.java
+++ src/Model/Coin.java
@@ -1,36 +1,70 @@
 package Model;
 
+/**
+ * Représente les 4 différents coins d'un Twist-Lock
+ *
+ * @author Groupe109
+ */
+
 public class Coin
-{//
+{
     private boolean occupe;
     private Joueur occupant;
 
+    /**
+     * Constructeur de la classe Coin
+     */
     public Coin()
     {
         occupe = false;
     }
 
+    /**
+     * Accesseur au booleen occupant
+     *
+     * @return occupant
+     */
     public Joueur getOccupant()
     {
         return occupant;
     }
 
+    /**
+     * Modificateur de l'occupant en changeant le booleen occupe
+     *
+     * @param occupant
+     */
     public void setOccupant(Joueur occupant)
     {
         this.occupant = occupant;
         this.occupe = true;
     }
 
+    /**
+     * Vérifie si un coin est occupé
+     *
+     * @return
+     */
     public boolean isOccupe()
     {
         return occupe;
     }
 
+    /**
+     * Modificateur du booleen occupe
+     *
+     * @param occupe
+     */
     public void setOccupe(boolean occupe)
     {
         this.occupe = occupe;
     }
 
+    /**
+     * Méthode de mise en forme de la classe Coin
+     *
+     * @return
+     */
     public String toString()
     {
         if (this.occupe)
